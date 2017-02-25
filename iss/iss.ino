ebd62518a9c1425bafbc53beb9874b057f4437c8
aurelien fache <aurelien.fache@gmail.com>
2017-02-25 18:03:10 +0100

    add some documentation

--- iss/iss.ino
+++ iss/iss.ino
@@ -1,10 +1,10 @@
 String str;
 String command;
 String val_nb_step;
-int delay_step = 1000;
+int delay_m = 2000;
 
 void setup() {
-  //moteur des Y (celui du bat)
+  //moteur des Y (celui du bas)
   pinMode(6, OUTPUT);
   pinMode(7, OUTPUT);
   digitalWrite(6, HIGH);//pin de direction HIGH monte dans les Y lOW descend
@@ -19,57 +19,49 @@ void setup() {
   Serial.begin(9600);
 }
 
+
 void mv_right(int nb_step) {
-    digitalWrite(8, LOW);// move to right 
-    for (int i=0; i <= nb_step; i++){
+    digitalWrite(8, LOW);//set pin direction to move to right 
+    for (int i=0; i <= nb_step; i++){//pin step
       digitalWrite(9, HIGH);
-      //delay(delay_step);
-      delayMicroseconds(500);          
-     // delayMicroseconds
+      delayMicroseconds(delay_m);          
       digitalWrite(9, LOW);
-      //delay(delay_step);
-       delayMicroseconds(500);         
+      delayMicroseconds(delay_m);         
     }
-  
 }
 
+//move left
 void mv_left(int nb_step) {
-    digitalWrite(8, HIGH); // move to left 
-    for (int i=0; i <= nb_step; i++){
+    digitalWrite(8, HIGH); // set pin direction to move to left 
+    for (int i=0; i <= nb_step; i++){//pin step
       digitalWrite(9, HIGH);
-     // delay(delay_step);
-       delayMicroseconds(500);  
+      delayMicroseconds(delay_m);  
       digitalWrite(9, LOW);
-     // delay(delay_step);
-       delayMicroseconds(500);  
+      delayMicroseconds(delay_m);  
     }
-  
 }
 
 void mv_up(int nb_step) {
     digitalWrite(6, HIGH); // move to UP 
     for (int i=0; i <= nb_step; i++){
       digitalWrite(7, HIGH);
-     // delay(delay_step);
-       delayMicroseconds(500);  
+      delayMicroseconds(delay_m);  
       digitalWrite(7, LOW);
-     // delay(delay_step);
-       delayMicroseconds(500);  
+      delayMicroseconds(delay_m);  
     }
 }
 
 void mv_down(int nb_step) {
-    digitalWrite(6, LOW); // move to DONW 
+   digitalWrite(6, LOW); // move to DONW 
    for (int i=0; i <= nb_step; i++){
       digitalWrite(7, HIGH);
-     // delay(delay_step);
-       delayMicroseconds(500);  
+      delayMicroseconds(delay_m);  
       digitalWrite(7, LOW);
-     // delay(delay_step);
-       delayMicroseconds(500);  
+      delayMicroseconds(delay_m);  
     }
 }
 
+//function split input command 
 String getValue(String data, char separator, int index)
 {
     int found = 0;
@@ -86,24 +78,34 @@ String getValue(String data, char separator, int index)
     return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
 }
 
+//read input command like 
+// r=100, l=100,u=100,d=100
 void loop() {
   if(Serial.available()) {
     str = Serial.readStringUntil('\n');
     command = getValue(str,'=',0);
     val_nb_step = getValue(str,'=',1);
-    Serial.println(command);
-    Serial.println(val_nb_step);
+    //Serial.println(command);
+    
      if ( command == "l") {
+       Serial.print("move left");
        mv_left(val_nb_step.toInt());
      }
      if ( command == "u") {
+       Serial.print("move up");
        mv_up(val_nb_step.toInt());
      }
      if ( command == "d") {
+        Serial.print("move down");
        mv_down(val_nb_step.toInt());
      }
      if ( command == "r") {
+        Serial.print("move right");
        mv_right(val_nb_step.toInt());
      }
+     Serial.print(" with nb step : ");
+     Serial.println(val_nb_step);
+     Serial.println("-----------------");
+     
   }
 }
